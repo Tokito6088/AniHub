3acd33983f3ccd06fb842579cd85cb5a9bb35c8e
yahya momin <yahyamomin06@gmail.com>
2024-06-02 14:19:20 +0530

    update

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,38 +1,29 @@
-import React from 'react';
+import React from 'react'
 
-import ContentWrapper from '../contentWrapper/ContentWrapper';
+import ContentWrapper from '../contentWrapper/ContentWrapper'
 
-import './style.scss';
+import './style.scss'
 
 const Footer = () => {
-	return (
-		<footer className='footer'>
-			<ContentWrapper>
-				<div className='infoText'>this is AniHub Anime Web Application Using react Redux And jikan api </div>
-				<div className='infoText'>
-					Contect Me Here - <code>Yahyamomin06@gmail.com</code>
-				</div>
-				<div className='infoText'>@ copy right all rights reserved - Yahya</div>
-				<ul className='links'>
-					<li>
-						<a href='https://github.com/Tokito6088' target='_blank'>
-							<i className='fa-brands fa-github'></i>
-						</a>
-					</li>
-					<li>
-						<a href='https://www.linkedin.com/in/yahya-momin-53794225a' target='_blank'>
-							<i className='fa-brands fa-linkedin'></i>
-						</a>
-					</li>
-				</ul>
-				<div className='getCode'>
-					<a href='https://github.com/Tokito6088/ClineHub' target='_blank'>
-						Get Code
-					</a>
-				</div>
-			</ContentWrapper>
-		</footer>
-	);
-};
+  return (
+    <footer className="footer">
+      <ContentWrapper>
+        <ul className="links">
+          <li>
+            <a href="https://github.com/yahyaMomin" target="_blank" rel="noreferrer">
+              <i className="fa-brands fa-github"></i>
+            </a>
+          </li>
+          <li>
+            <a href="https://www.linkedin.com/in/yahya-momin-53794225a" target="_blank" rel="noreferrer">
+              <i className="fa-brands fa-linkedin"></i>
+            </a>
+          </li>
+        </ul>
+        <div className="infoText">@ copyRights all rights reserved - AniHub</div>
+      </ContentWrapper>
+    </footer>
+  )
+}
 
-export default Footer;
+export default Footer
--- src/pages/home/herobanner/HeroBanner.jsx
+++ src/pages/home/herobanner/HeroBanner.jsx
@@ -1,49 +1,56 @@
-import React, { useEffect, useState } from 'react';
-import './style.scss';
-import { useNavigate } from 'react-router-dom';
-import useFetch from '../../../hooks/usefetch';
-import { useSelector } from 'react-redux';
-import Img from '../../../components/lazyLoadImage/Image';
-import ContentWrapper from '../../../components/contentWrapper/ContentWrapper';
-import axios from 'axios';
+import React, { useEffect, useState } from 'react'
+import './style.scss'
+import { useNavigate } from 'react-router-dom'
+import useFetch from '../../../hooks/usefetch'
+import { useSelector } from 'react-redux'
+import Img from '../../../components/lazyLoadImage/Image'
+import ContentWrapper from '../../../components/contentWrapper/ContentWrapper'
+import axios from 'axios'
 
 const HeroBanner = () => {
-	const { url } = useSelector((state) => state.Home);
+  const { url } = useSelector((state) => state.Home)
 
-	const [background, setBackground] = useState('');
-	const [query, setQuery] = useState('');
-	const navigate = useNavigate();
+  const [background, setBackground] = useState('')
+  const [query, setQuery] = useState('')
+  const navigate = useNavigate()
 
-	useEffect(() => {
-		const bg = url;
-		setBackground(bg);
-	}, [url]);
+  useEffect(() => {
+    const bg = url
+    setBackground(bg)
+  }, [url])
 
-	const searchQueryHandler = (event) => {
-		if (event.key == 'Enter' && query.length > 0) {
-			event.preventDefault();
-			navigate(`explore/${query}`);
-		}
-	};
-	return (
-		<div className='heroBanner'>
-			<div className='backdrop-img'>
-				<Img src={background} />
-			</div>
-			<div className='opacity-layer'></div>
-			<ContentWrapper>
-				<div className='wrapper'>
-					<div className='heroBannerContent'>
-						<span className='subtitle'>World Larges Anime DataBase</span>
-						<div className='heroBannerSearchInput'>
-							<input type='text' placeholder='search for anime or manga....' onKeyUp={searchQueryHandler} onChange={(e) => setQuery(e.target.value)} />
-							<button onClick={() => navigate(`/search/${query}`)}>Search</button>
-						</div>
-					</div>
-				</div>
-			</ContentWrapper>
-		</div>
-	);
-};
+  const searchQueryHandler = (event) => {
+    if (event.key == 'Enter' && query.length > 0) {
+      event.preventDefault()
+      navigate(`explore/${query}`)
+    }
+  }
+  return (
+    <div className="heroBanner">
+      <div className="backdrop-img">
+        <Img src={background} />
+      </div>
+      <div className="opacity-layer"></div>
+      <ContentWrapper>
+        <div className="wrapper">
+          <div className="heroBannerContent">
+            <span className="subtitle">World Larges Anime DataBase</span>
+            <form className="heroBannerSearchInput">
+              <input
+                type="text"
+                placeholder="search for anime or manga...."
+                onKeyUp={searchQueryHandler}
+                onChange={(e) => setQuery(e.target.value)}
+              />
+              <button type="button" onClick={() => navigate(`/explore/${query}`)}>
+                Search
+              </button>
+            </form>
+          </div>
+        </div>
+      </ContentWrapper>
+    </div>
+  )
+}
 
-export default HeroBanner;
+export default HeroBanner
